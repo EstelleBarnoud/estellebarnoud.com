3cf9d0db6ee0c5592083dfc49e86eb04e007fc82
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2019-09-20 19:51:31 -0400

    Removed useless code - window props not need out of an iframe

--- client/src/AppBar.js
+++ client/src/AppBar.js
@@ -10,11 +10,8 @@ import Container from '@material-ui/core/Container';
 import Slide from '@material-ui/core/Slide';
 
 function HideOnScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+  const { children } = props;
+  const trigger = useScrollTrigger();
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
@@ -25,11 +22,6 @@ function HideOnScroll(props) {
 
 HideOnScroll.propTypes = {
   children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
 };
 
 export default function HideAppBar(props) {
