2b8f83e85505030cc1f6b16edfd1ae91ed36fb59
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2019-09-21 14:28:43 -0400

    Added AppBar buttons: About me, Projects

--- client/src/AppBar.js
+++ client/src/AppBar.js
@@ -6,6 +6,18 @@ import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+    bar: {
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    buttons: {
+        //display: "flex",
+        //justifyContent: "space-around"
+    },
+}
 
 function HideOnScroll(props) {
   const { children } = props;
@@ -28,8 +40,12 @@ export default function HideAppBar(props) {
       <CssBaseline />
       <HideOnScroll {...props}>
         <AppBar>
-          <Toolbar>
+          <Toolbar style={styles.bar}>
             <Typography variant="h6">Hello World!</Typography>
+            <div style={styles.buttons}>
+                <Button color="inherit">About me</Button>
+                <Button color="inherit">Projects</Button>
+            </div>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
--- client/src/Cover.js
+++ client/src/Cover.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import cover from './cover.JPG';
 
-export default function HideAppBar(props) {
+export default function Cover(props) {
   return (
       <img src={cover} className="App-cover" alt="cover" />
   )
