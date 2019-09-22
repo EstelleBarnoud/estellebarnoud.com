9133abfd67cb09d126d33d0f26988b92b830aba8
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2019-09-22 11:45:38 -0400

    Improved code visibility by regrouping imports from Material-UI

--- client/src/AppBar.js
+++ client/src/AppBar.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Slide from '@material-ui/core/Slide';
-import Button from '@material-ui/core/Button';
+import { AppBar, Toolbar, Typography, CssBaseline, useScrollTrigger, Slide, Button } from '@material-ui/core';
 
 const styles = {
     bar: {
