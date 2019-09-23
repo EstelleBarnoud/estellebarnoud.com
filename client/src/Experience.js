08f5c52ba91825815d59f412176891a2da9f9081
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2019-09-22 20:40:12 -0400

    Added icons to tab items

--- client/src/Experience.js
+++ client/src/Experience.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography, Box, Tabs, Tab } from '@material-ui/core';
+import { Typography, Box, Tabs, Tab, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import SchoolIcon from '@material-ui/icons/School';
+import WorkIcon from '@material-ui/icons/BusinessCenter';
+import NatureIcon from '@material-ui/icons/Eco';
+import SportIcon from '@material-ui/icons/FitnessCenter';
+import ToolIcon from '@material-ui/icons/Build';
+import HeartIcon from '@material-ui/icons/Favorite';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -62,15 +68,17 @@ export default function Experience() {
                 aria-label="Vertical tabs example"
                 className={classes.tabs}
             >
-                <Tab label="Education" {...a11yProps(0)} />
-                <Tab label="Career" {...a11yProps(1)} />
-                <Tab label="Volunteering" {...a11yProps(2)} />
-                <Tab label="Skills" {...a11yProps(3)} />
-                <Tab label="Projects" {...a11yProps(4)} />
-                <Tab label="Passions" {...a11yProps(5)} />
+                <Tab icon={<SchoolIcon />} label="Education" {...a11yProps(0)} />
+                <Tab icon={<WorkIcon />} label="Career" {...a11yProps(1)} />
+                <Tab icon={<NatureIcon />} label="Volunteering" {...a11yProps(2)} />
+                <Tab icon={<SportIcon />} label="Skills" {...a11yProps(3)} />
+                <Tab icon={<ToolIcon />} label="Projects" {...a11yProps(4)} />
+                <Tab icon={<HeartIcon />} label="Passions" {...a11yProps(5)} />
             </Tabs>
             <TabPanel value={value} index={0}>
-                Item One
+                <Container>
+                    Columbia University in the City of New York
+                </Container>            
             </TabPanel>
             <TabPanel value={value} index={1}>
                 Item Two
