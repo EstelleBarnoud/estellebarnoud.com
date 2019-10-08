86ae3f5e90207dd27de7786ebbcab44f111e833d
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2019-10-08 13:16:43 -0400

    Added footnote with powered by node.js

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -3,14 +3,29 @@ import { Container, IconButton, Tooltip } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import ResumeIcon from '@material-ui/icons/Description';
-import GitHub from './Github.js';
+import GitHubIcon from './Github.js';
+import NodeJsIcon from './images/nodejs.png';
 
 const styles = {
-    container: {
+    icons: {
         display: 'flex',
         justifyContent: 'space-around',
         padding: '5%',
-    }
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    node: {
+        maxHeight: '40px',
+        margin: '0% 5%',
+    },
+    tech: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '2%',
+    },
 }
 
 const sendEmail = () =>
@@ -19,26 +34,32 @@ const sendEmail = () =>
 export default function Contact(){
     return(
         <Container id="contact" style={styles.container} >
-            <Tooltip title="LinkedIn profile" >
-                <IconButton href="https://www.linkedin.com/in/estellebarnoud/?locale=en_US" target="_blank" rel="noopener noreferrer" >
-                    <LinkedInIcon fontSize="large" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="GitHub account">
-                <IconButton href="https://github.com/EstelleBarnoud/" target="_blank" rel="Norefferer" >
-                    <GitHub />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Send an email">
-                <IconButton onClick={sendEmail}>
-                    <EmailIcon fontSize="large" />
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="See Resume" >
-                <IconButton href='/resume.pdf' target="_blank" rel="noreferrer">
-                    <ResumeIcon fontSize="large" />
-                </IconButton>
-            </Tooltip>
+            <Container style={styles.icons} >
+                <Tooltip title="LinkedIn profile" >
+                    <IconButton href="https://www.linkedin.com/in/estellebarnoud/?locale=en_US" target="_blank" rel="noopener noreferrer" >
+                        <LinkedInIcon fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="GitHub account">
+                    <IconButton href="https://github.com/EstelleBarnoud/" target="_blank" rel="Norefferer" >
+                        <GitHubIcon />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Send an email">
+                    <IconButton onClick={sendEmail}>
+                        <EmailIcon fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="See Resume" >
+                    <IconButton href='/resume.pdf' target="_blank" rel="noreferrer">
+                        <ResumeIcon fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+            </Container>
+            <Container style={styles.tech}>
+                <p>Powered by</p>
+                <img src={NodeJsIcon} style={styles.node} alt="NodeJs icon"/>
+            </Container>    
         </Container>
     )
 }
