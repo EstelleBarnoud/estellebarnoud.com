e2e3e60a0b4559c164da37194b785298f1d6acba
EstelleBarnoud <estelle.barnoud@telecom-paristech.fr>
2020-04-26 15:06:22 -0400

    Added Muitheme to override font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import AppBar from './AppBar.js';
 import Contact from './Contact.js';
 import About from './About.js';
@@ -8,28 +8,34 @@ import './App.css';
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      //light: '#757ce8',
-      main: '#8BBBEB',
-      //dark: '#002884',
-      //contrastText: '#fff',
-    },
-    secondary: {
-      main: '#C51162',
-    },
+    // primary: {
+    //   //light: '#757ce8',
+    //   main: '#8BBBEB',
+    //   //dark: '#002884',
+    //   //contrastText: '#fff',
+    // },
+    // secondary: {
+    //   main: '#C51162',
+    // },
   },
+  typography: {
+    fontFamily: "ABeeZee",
+    align: "center",
+  }
 });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AppBar />
-      </header>
-      <About />
-      <Experience />
-      <Contact />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <AppBar />
+        </header>
+        <About />
+        <Experience />
+        <Contact />
+      </div>
+    </MuiThemeProvider>
   );
 }
 
