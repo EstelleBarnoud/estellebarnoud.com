e933799d82195ce433ba94f5850b0731aac77e9c
Estelle (Barnoud) Wolski <28601386+EstelleBarnoud@users.noreply.github.com>
2022-08-12 12:55:42 +0200

    Updated work experience

--- src/WorkExp.js
+++ src/WorkExp.js
@@ -4,6 +4,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
+import gojob from './images/gojob.png';
 import loreal from './images/loreal.jpg';
 import cybelangel from './images/cybelangel2.png';
 import gates from './images/gates-foundation.webp';
@@ -24,6 +25,21 @@ function createData(company, title, description) {
 
 const rows = [
   createData(
+    <a style={styles.img} href="https://www.gojob.com/" target="_blank" rel="noopener noreferrer" >
+        <img src={gojob} style={styles.img} alt="Gojob" />
+    </a>,
+    <div>
+        <p>Senior Analytics Engineer &amp; Data Engineer</p>
+        <i> Full time</i>
+    </div>,
+    <ul>
+        <li>Reduced # of customer escalations from 20 to 2 per month by migrating data models from old stack <i>[Airflow][DOMO]</i> to new <i>[Fivetran][BigQuery][dbt]</i></li>
+        <li>Cut # of incidents by half by setting up automatic tests both on a daily basis to proactively fix data quality issues and on code changes for continuous integration <i>[GitLab][dbt]</i></li>
+        <li>Increased analytics team impact by setting up a technical roadmap, recruiting 4 engineers, producing self-service documentation and upskilling via code reviews &amp; mentoring</li>
+        <li>Served on-call rotations on 10+ data sources &amp; 150+ models</li>
+    </ul>
+    ),
+  createData(
     <a style={styles.img} href="https://www.loreal.com/" target="_blank" rel="noopener noreferrer" >
         <img src={loreal} style={styles.img} alt="L'Oreal" />
     </a>,
@@ -32,9 +48,9 @@ const rows = [
         <i> Full time</i>
     </div>,
     <ul>
-        <li>Designing hierarchical Bayes estimation with choice-based conjoint (CBC + HB) research to understand customer behavior and optimize brands portfolio &amp; pricing</li>
-        <li>Using Machine Learning (e.g. Text Mining) to provide recommendations based on customer insight</li>
-        <li>Automating processes (e.g. scraping, design tools) to boost RGM efficiency <i>[Python] [VBA]</i></li>
+        <li>Designed hierarchical Bayes estimation with choice-based conjoint research to understand customer behavior – saved $10M+ in portfolio &amp; pricing optimization</li>
+        <li>Used Machine Learning to provide quantitative &amp; qualitative recommendations based on customer insight</li>
+        <li>Automated processes to boost capabilities – optimized research designing from 5 days to 1 <i>[Python] [VBA]</i></li>
     </ul>
     ),
   createData(
@@ -47,7 +63,7 @@ const rows = [
     </div>,
     <ul>
         <li>Prepared business data &amp; slides for pitch deck to raise $36M in series B round</li>
-        <li>Analyzed &amp; forecasted sales performances to provide data-driven insights; implemented tools to automate data analyses</li>
+        <li>Analyzed &amp; forecast marketing and sales performance to recommend data-driven strategies</li>
         <li>Worked cross-teams on systematic tracking of KPIs; optimized sales &amp; marketing processes</li>
     </ul>
     ),
@@ -60,8 +76,8 @@ const rows = [
         <i> Part time</i>
     </div>,
     <ul>
-        <li>Created statistical machine learning robots to build linguistic glossaries <i>[Python]</i></li>
-        <li>Implemented recurrent neural network to translate sentence-to-sentence <i>[TensorFlow][Python]</i></li>
+        <li>Applied statistical machine learning models to build linguistic glossaries <i>[Python]</i></li>
+        <li>Implemented recurrent neural network to translate languages with limited data <i>[TensorFlow][Python]</i></li>
     </ul>),
   createData(
     <a style={styles.img} href="www.bluecloudventures.com/" target="_blank" rel="noopener noreferrer" >
@@ -72,7 +88,7 @@ const rows = [
         <i> Part time</i>
     </div>,
     <ul style={styles.list}>
-        <li>Automated search of new startups to invest in through ranking dashboard tool <i>[scraping][Power BI]</i></li>
+        <li>Automated search of new startups to invest in through ranking dashboard tool <i>[Python][Power BI]</i></li>
     </ul>
     ),
   
