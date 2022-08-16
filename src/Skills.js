48eb1cb0a6833a9d76849c96d476b05c653d7de9
Estelle (Barnoud) Wolski <28601386+EstelleBarnoud@users.noreply.github.com>
2022-08-16 11:55:36 -0400

    Updated Skill section

--- src/Skills.js
+++ src/Skills.js
@@ -19,35 +19,47 @@ const styles = {
 export default function Skills() {
     return (
         <div>
-            <p style={styles.categories}>Coding, Machine Learning &amp; Stats</p>
+            <p style={styles.categories}>Programming</p>
             <div style={styles.root}>
                 <Chip variant='outlined' label='Python' style={styles.chip} />
-                <Chip variant='outlined' label='sklearn' style={styles.chip} />
-                <Chip variant='outlined' label='pandas' style={styles.chip} />
-                <Chip variant='outlined' label='R' style={styles.chip} />
-                <Chip variant='outlined' label='Java' style={styles.chip} />
+                <Chip variant='outlined' label='SQL' style={styles.chip} />
             </div>
-            <p style={styles.categories}>OS &amp; Version Control</p>
+            <p style={styles.categories}>Analytics engineering &amp; data pipelines</p>
             <div style={styles.root}>
-                <Chip variant='outlined' label='Linux' style={styles.chip} />
-                <Chip variant='outlined' label='Windows' style={styles.chip} />
-                <Chip variant='outlined' label='Git' style={styles.chip} />
+                <Chip variant='outlined' label='dbt' style={styles.chip} />
+                <Chip variant='outlined' label='BigQuery' style={styles.chip} />
+                <Chip variant='outlined' label='Airflow' style={styles.chip} />
+                <Chip variant='outlined' label='Fivetran' style={styles.chip} />
             </div>
-            <p style={styles.categories}>Web Development</p>
+            <p style={styles.categories}>DevOps</p>
+            <div style={styles.root}>
+                <Chip variant='outlined' label='git' style={styles.chip} />
+                <Chip variant='outlined' label='GitLab CI' style={styles.chip} />
+                <Chip variant='outlined' label='Docker' style={styles.chip} />
+            </div>
+            <p style={styles.categories}>Data Visualization &amp; Analysis</p>
+            <div style={styles.root}>
+                <Chip variant='outlined' label='Power BI' style={styles.chip} />
+                <Chip variant='outlined' label='Tableau' style={styles.chip} />
+                <Chip variant='outlined' label='Excel' style={styles.chip} />
+                <Chip variant='outlined' label='VBA' style={styles.chip} />  
+            </div>
+            <p style={styles.categories}>Data sourcing (databases, events &amp; SaaS)</p>
+            <div style={styles.root}>
+                <Chip variant='outlined' label='PostgreSQL' style={styles.chip} />
+                <Chip variant='outlined' label='MongoDB' style={styles.chip} />
+                <Chip variant='outlined' label='Salesforce' style={styles.chip} />
+                <Chip variant='outlined' label='HubSpot' style={styles.chip} />
+                <Chip variant='outlined' label='Google Analytics' style={styles.chip} />
+                <Chip variant='outlined' label='AirTable' style={styles.chip} />
+                <Chip variant='outlined' label='PubSub' style={styles.chip} />
+            </div>
+            <p style={styles.categories}>Web Development (side projects)</p>
             <div style={styles.root}>
                 <Chip variant='outlined' label='HTML' style={styles.chip} />
                 <Chip variant='outlined' label='CSS' style={styles.chip} />
                 <Chip variant='outlined' label='Javascript' style={styles.chip} />
                 <Chip variant='outlined' label='React' style={styles.chip} />
-                <Chip variant='outlined' label='Redux' style={styles.chip} />
-            </div>
-            <p style={styles.categories}>Data Management, Visualization &amp; Analysis</p>
-            <div style={styles.root}>
-                <Chip variant='outlined' label='SQL' style={styles.chip} />
-                <Chip variant='outlined' label='Excel' style={styles.chip} />
-                <Chip variant='outlined' label='Power BI' style={styles.chip} />
-                <Chip variant='outlined' label='Tableau' style={styles.chip} />
-                <Chip variant='outlined' label='Salesforce' style={styles.chip} />  
             </div>
         </div>
     );
